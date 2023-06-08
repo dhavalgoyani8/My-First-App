3f275cd46b6b0ab17aaef60ff068030069fd723b
Dhaval <goyanidhaval8@gmail.com>
2023-06-08 09:49:44 +0530

    Change home file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
-
+import { useState } from "react";
 
 
 function Home() {
 
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <h1>this is home page</h1>   
+      <h1>this is home page</h1>
+         <h1>{count}</h1>
+         <button onClick={()=>setCount(count - 1)}>Decrement</button>
+         <button onClick={()=>setCount(count + 1)}>Increment</button>
     </>
   );
 }
